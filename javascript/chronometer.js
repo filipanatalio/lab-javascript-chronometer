59016ee07b74680de92fd16098c49e999a69a0b7
filipanatalio <87820213+filipanatalio@users.noreply.github.com>
2022-01-24 18:49:12 +0000

    last

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,53 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
     // ... your code goes here
+
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      // if (callback > ) {
+      //   clearInterval(intervalId2);
+      // }
+    }, 1000);
   }
 
   getMinutes() {
     // ... your code goes here
+
+    return Math.trunc(this.currentTime / 60);
   }
 
   getSeconds() {
+    return this.currentTime % 60;
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    return ('0' + value).slice(-2);
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.currentTime);
+    clearInterval(this.intervalId);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
     // ... your code goes here
+    return `${this.computeTwoDigitNumber(
+      this.getMinutes()
+    )}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
 
